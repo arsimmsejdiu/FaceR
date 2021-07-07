fdb13ac08f07883379b617557f93d9a3c866604d
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-07-07 15:31:13 +0200

    Added User Rank

--- pages/components/Rank/Rank.jsx
+++ pages/components/Rank/Rank.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const Rank = () => {
   return (
     <div>
-      <p className="f3 center">
-        {"Your Rank"}
-      </p>
+      <div className="f3 center white">
+        {"Arsim, your current rank is ..."}
+      </div>
+      <div className="f1 center white">
+        {"#5"}
+      </div>
     </div>
   );
 };
