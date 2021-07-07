75b8dc763b5c5ca2db6152e397f2a099b0701381
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-07-07 16:16:35 +0200

    added particle background

--- pages/components/footer.jsx
+++ pages/components/footer.jsx
@@ -1,19 +1,21 @@
 //import Image from 'next/image'
-import styles from '../../styles/Home.module.css'
+import styles from "../../styles/Home.module.css";
 
 export default function Footer() {
   return (
+    <main>
       <footer className={styles.footer}>
         <a
           href="https://arsimsejdiu.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by Arsim Sejdiu{' ' + new Date().getFullYear() + ' '}
+          Powered by Arsim Sejdiu{" " + new Date().getFullYear() + " "}
           {/* <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span> */}
         </a>
       </footer>
-  )
+    </main>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import 'tachyons';
+import Particles from "react-particles-js";
+import "tachyons";
 
 import Footer from "./components/footer";
 import Navigation from "./components/Navigation/Navigation";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3ca9d1",
+        blur: 5,
+      },
+    },
+   number: {
+     value: 80,
+     density: {
+       enable: true,
+       value_area: 800
+     }
+   }
+  },
+};
 
 export default function Home() {
   return (
@@ -15,15 +35,20 @@ export default function Home() {
         <meta name="description" content="Face Recognition App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Particles
+        className="particles"
+        params={particlesOptions}
+      />
 
       <Navigation />
       <Rank />
       <ImageLinkForm />
-        {/*
+      {/*
         <FaceRecognition /> */}
-      <div className={styles.container}>
+      {/* <div className={styles.container}>
         <Footer />
-      </div>
+      </div> */}
+        
     </div>
   );
 }
